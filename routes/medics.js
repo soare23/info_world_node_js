0dd50e08dacb4e7d32ee016f5a665a0aac0fb2ff
soare23 <toma.mihai.sorin23@gmail.com>
2021-04-13 16:23:30 +0300

    done except A.3 and B.2

--- routes/medics.js
+++ routes/medics.js
@@ -7,56 +7,78 @@ const fs = require('fs');
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/csv-upload', upload.single('csv'), (req, res) => {
-  if (req.headers['content-type'].includes('multipart/form-data')) {
-    let content = fs.readFileSync(req.file.path, 'binary');
-    fs.unlink(req.file.path, () => {
-      const rows = content.split('\n');
-      const rawData = rows.map((d) => d.split(','));
-      const headers = rawData[0];
-      const data = rawData.slice(1);
-      const medics = data.map((row) => {
-        const obj = {};
-        headers.forEach((h, i) => {
-          let trimmed = h.trim();
-          let cammelCase = trimmed.charAt(0).toLowerCase() + trimmed.slice(1);
-          obj[cammelCase] = row[i].trim();
+  // Generate mock token
+  let buff = new Buffer.from(req.headers.authorization, 'base64');
+  let decoded = buff.toString('ascii');
+  let tokenObject = JSON.parse(decoded);
+
+  if (
+    tokenObject.roles.includes('Admin') ||
+    tokenObject.roles.includes('Practitioner')
+  ) {
+    // CSV file
+    if (req.headers['content-type'].includes('multipart/form-data')) {
+      let content = fs.readFileSync(req.file.path, 'binary');
+      fs.unlink(req.file.path, () => {
+        const rows = content.split('\n');
+        const rawData = rows.map((d) => d.split(','));
+        const headers = rawData[0];
+        const data = rawData.slice(1);
+        const medics = data.map((row) => {
+          const obj = {};
+          headers.forEach((h, i) => {
+            let trimmed = h.trim();
+            let cammelCase = trimmed.charAt(0).toLowerCase() + trimmed.slice(1);
+            obj[cammelCase] = row[i].trim();
+          });
+          return obj;
         });
-        return obj;
-      });
+        let hospitalsByMedic = {};
 
-      let activeHospitalsByMedics = {
-        medicName: '',
-        hospitals: '',
-      };
+        medics.forEach((medic) => {
+          if (tokenObject.facility.includes(medic.facilityId)) {
+            let medicName = medic.familyName + ' ' + medic.givenName;
+            hospitalsByMedic[medicName] = hospitalsByMedic[medicName] || {
+              hospitals: [],
+            };
 
-      console.log(medics);
+            if (medic.active === 'true') {
+              // push hospital to the list of hospitals of the medic with id 'medic.id' where he is active
+              hospitalsByMedic[medicName].hospitals.push(medic.nameId);
+            }
+          }
+        });
 
-      medics.forEach((medic) => {
-        let hospitalsByActiveMedic = [];
-        let medicName = medic.familyName + ' ' + medic.givenName;
-
-        if (medic.active === 'true') {
-          activeHospitalsByMedics.medicName = medicName;
-          hospitalsByActiveMedic.push(medic.nameId);
-          activeHospitalsByMedics[medicName] = hospitalsByActiveMedic;
+        for (const [key, value] of Object.entries(hospitalsByMedic)) {
+          console.log(key + ': ' + value.hospitals);
         }
       });
 
-      for (let key in activeHospitalsByMedics) {
-        console.log(key, activeHospitalsByMedics[key]);
-      }
-    });
-
-    res.status(200).send('CSV FILE READ');
-  } else {
-    if (!req.body.id || req.body.resourceType !== 'Practitioner') {
-      console.log('error');
+      res.status(200).send('CSV FILE READ');
     } else {
-      if (req.body.active) {
-        console.log(req.body.name);
-        console.log(req.body.facility);
+      // standard JSON
+      if (!req.body.id || req.body.resourceType !== 'Practitioner') {
+        console.log('You are not authorized to access this.');
+        res.status(401).send('You are not authorized to access this.');
+      } else {
+        if (req.body.active) {
+          let selectedFacilities = [];
+          req.body.facility.forEach((facility) => {
+            if (tokenObject.facility.includes(facility.value)) {
+              selectedFacilities.push(facility);
+            }
+          });
+
+          selectedFacilities.forEach((facility) => {
+            console.log(req.body.name[0].text + ': ' + facility.name);
+          });
+          res.status(200).send();
+        }
       }
     }
+  } else {
+    console.log('You are not authorized to access this.');
+    res.status(401).send('You are not authorized to access this.');
   }
 });
 
